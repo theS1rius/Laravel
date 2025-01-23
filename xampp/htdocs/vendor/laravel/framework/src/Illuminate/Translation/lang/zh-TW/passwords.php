0fb8b335c8d4378c23cb544fcc08520542a21fc1
Sirius <129046964+theS1rius@users.noreply.github.com>
2025-01-23 10:07:25 +0800

    Add files via upload

---
+++ xampp/htdocs/vendor/laravel/framework/src/Illuminate/Translation/lang/zh-TW/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => '您的密碼已被重設。',
+    'sent' => '我們已經透過電子郵件發送您的密碼重設連結。',
+    'throttled' => '請稍候再試。',
+    'token' => '此密碼重設令牌無效。',
+    'user' => '我們無法找到使用該電子郵件地址的用戶。',
+
+];
